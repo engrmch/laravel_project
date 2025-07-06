e1b8d9ee102894bd42b7236aaf55a5560c65ef37
engrmch <michelle.pamin@gmail.com>
2025-07-06 22:23:36 +0800

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,4 @@ cd /var/www/html/laravel_project
 composer install --no-dev --optimize-autoloader
 
 php artisan migrate --force
-
-sudo chown -R apache:apache storage bootstrap/cache
-sudo chmod -R 775 storage bootstrap/cache
-
 php artisan config:cache
-php artisan route:cache
-
-sudo systemctl restart httpd
